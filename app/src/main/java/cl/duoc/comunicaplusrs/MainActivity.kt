9fe9bf83de3d190be5f4e7a8d254a7f16395886e
Roberto Sánchez F. <roberto.asf@gmail.com>
2026-08-24 22:55:00 -0400

    correcion de MainActivity.kt

--- app/src/main/java/cl/duoc/comunicaplusrs/MainActivity.kt
+++ app/src/main/java/cl/duoc/comunicaplusrs/MainActivity.kt
@@ -3,14 +3,6 @@ package cl.duoc.comunicaplusrs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import cl.duoc.comunicaplusrs.ui.theme.ComunicaPlusRSTheme
 import cl.duoc.comunicaplusrs.navigation.AppNavigation
 
@@ -29,12 +21,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hellooo $name!",
-        modifier = modifier
-    )
 }
